34a582bf3cab232837f00e06322024d450b24969
miguelsabino23 <miguel2012palhada@gmail.com>
2024-09-28 17:48:06 -0300

    style: adicionando tema padrão

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,22 @@
+export const defaultTheme = {
+  produto: {
+    yellow_dark: "#c47f17",
+    yellow: "#dbac2c",
+    yellow_light: "f1e9c9",
+    puple_dark: "#4b2995",
+    purple: "#8047f8",
+    purple_light: "ebe5f9",
+  },
+  base: {
+    base_title: "#272221",
+    base_subtitle: "#403937",
+    base_text: "#574f4d",
+    base_label: "#8d8686",
+    base_hover: "#d7d5d5",
+    base_button: "#e6e5e5",
+    base_input: "#ededed",
+    base_card: "#f3f2f2",
+    background: "#fafafa",
+    white: "#ffffff",
+  },
+};
