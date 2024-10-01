36ec4ffa6993785410a99bc69cd5b33d574aa6e0
miguelsabino23 <miguel2012palhada@gmail.com>
2024-10-01 19:33:59 -0300

    style: cria component de estilo layoutContainer

---
+++ src/layouts/DefaultLayout/style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const LayoutContainer = styled.div`
+  max-width: 62.5rem;
+
+  display: flex;
+  flex-direction: column;
+
+  margin: auto;
+  padding: 2rem 0;
+`;
