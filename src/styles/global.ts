7db7dd60b6c7103c339b2977c38f16c8527827af
miguelsabino23 <miguel2012palhada@gmail.com>
2024-10-01 19:34:30 -0300

    style: mudanças no estilo global

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,24 +1,25 @@
 import { createGlobalStyle } from "styled-components";
+import { mixins } from "./mixins";
 
 export const GlobalStyle = createGlobalStyle`
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-    :focus {
-        outline: none;
-    }
+  body {
+    background: ${(props) => props.theme.colors.background};
+    color: ${(props) => props.theme.colors["base-text"]};
+    -webkit-font-smoothing: antialiased;
+  }
 
-    body {
-        background-color: ${(props) => props.theme.base["background"]};
-    }
-    h1, h2, h3, h4, h5, h6 {
-        font-family: "Baloo 2", sans-serif;
-        ;
-    }
-    p, span, button, input {
-        font-family: "Roboto", sans-serif;
-    }
+  body, input, textarea, button {
+    ${mixins.fonts.textM};
+  }
+
+  button {
+    border: none;
+    cursor: pointer;
+  }
 `;
