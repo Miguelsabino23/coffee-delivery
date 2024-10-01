a30cad0f133a877e908192488271159e601a3f1e
miguelsabino23 <miguel2012palhada@gmail.com>
2024-10-01 19:35:40 -0300

    style: cria o tema padrão da página

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,22 +1,22 @@
 export const defaultTheme = {
-  produto: {
-    yellow_dark: "#c47f17",
-    yellow: "#dbac2c",
-    yellow_light: "f1e9c9",
-    puple_dark: "#4b2995",
-    purple: "#8047f8",
-    purple_light: "ebe5f9",
-  },
-  base: {
-    base_title: "#272221",
-    base_subtitle: "#403937",
-    base_text: "#574f4d",
-    base_label: "#8d8686",
-    base_hover: "#d7d5d5",
-    base_button: "#e6e5e5",
-    base_input: "#ededed",
-    base_card: "#f3f2f2",
-    background: "#fafafa",
-    white: "#ffffff",
+  colors: {
+    white: "#FFFFFF",
+    background: "#FAFAFA",
+    "base-card": "#F3F2F2",
+    "base-input": "#EDEDED",
+    "base-button": "#E6E6E5",
+    "base-hover": "#D7D5D5",
+    "base-label": "#8D8686",
+    "base-text": "#574F4D",
+    "base-subtitle": "#403937",
+    "base-title": "#272221",
+
+    "purple-light": "#EBE5F9",
+    purple: "#8047F8",
+    "purple-dark": "#4B2995",
+
+    "yellow-light": "#F1E9C9",
+    yellow: "#DBAC2C",
+    "yellow-dark": "#C47F17",
   },
 };
